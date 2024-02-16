fc92610980255e40c0df1913c7952abfbca8d21d
Your Name <you@example.com>
2024-02-16 13:47:04 +0530

    sidebar

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import Sidebar from "@/components/dashboard/sidebar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      main page    </main>
+      <Sidebar/>    </main>
   );
 }
--- src/components/dashboard/sidebar.js
+++ src/components/dashboard/sidebar.js
@@ -1,9 +1,76 @@
-import React from 'react'
-
-const sidebar = () => {
-  return (
-    <div>sidebar</div>
-  )
+'use client'
+import React, { useState } from 'react';
+import {
+    DashboardOutlined,
+  FormOutlined,
+  TableOutlined,
+  UserOutlined,
+  HighlightOutlined,
+} from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+const { Header, Content, Footer, Sider } = Layout;
+function getItem(label, key, icon, children) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  };
 }
+const items = [
+  getItem('Dashboard', '1', <DashboardOutlined />),
+  getItem('Project', '2', <FormOutlined />),
+  getItem('Employees', '3', <TableOutlined />, 
+  // [
+  //   // getItem('Tom', '3'),
+  //   // getItem('Bill', '4'),
+  //   // getItem('Alex', '5'),
+  // ]
+  ),
+  getItem('Preference', '4', <UserOutlined />, 
+  // [getItem('Team 1', '6'), getItem('Team 2', '8')]
+  ),
+  getItem('Compeliance', '5', <HighlightOutlined />),
+];
 
-export default sidebar
+  
+ 
+  
+
+const siderStyle = {
+  textAlign: 'left',
+  //lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#fff',
+  
+
+};
+const Sidebar = () => {
+  
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  return (
+    
+    
+    <Layout
+      style={{
+        minHeight: '100vh',
+        margin:'-85px',
+      }}
+    >
+      <Sider  style={siderStyle}  collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}
+      >
+        
+        <div  />
+        <Menu theme="light" defaultSelectedKeys={['1']} mode="inline" style={{
+                height: '100%',
+              }} items={items} />
+      </Sider>
+      
+    </Layout>
+    
+  );
+};
+export default Sidebar;
